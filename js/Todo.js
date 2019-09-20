1306401805e78eed7cc53dd1e19b3238064128de
bharanideepan <bharani@ideas2it.com>
2019-09-20 10:14:01 +0530

    Initil commit

---
+++ js/Todo.js
@@ -0,0 +1,23 @@
+function clickedField(id, value) {
+    if(value === "close") {
+        document.getElementById(id).value="open";
+        document.getElementById("nav-bar").style.width="295px";
+        var itemDescriptions = document.querySelectorAll(".item-description");
+        for (i = 0; i < itemDescriptions.length; i++) {
+            itemDescriptions[i].style.display = "block";
+        }
+    } else {
+        document.getElementById(id).value="close";
+        document.getElementById("nav-bar").style.width="55px";
+        var itemDescriptions = document.querySelectorAll(".item-description");
+        for (i = 0; i < itemDescriptions.length; i++) {
+            itemDescriptions[i].style.display = "none";
+        }
+    }
+}
+function mouseOverField(id) {
+    document.getElementById(id).style.border="1px solid #ededed";
+}
+function mouseOutField(id) {
+    document.getElementById(id).style.border="1px solid #f4f4f4";
+}
